2ffb69a862d193e3e59b35e09a2cf0c1b84f7b8c
ra138475 <ra138475@uem.br>
2025-05-21 19:13:48 +0000

    Busca por ID implementada

--- main.py
+++ main.py
@@ -12,6 +12,13 @@ class Filme:
     duracao:int
     elenco:str
 
+
+def redefinir_cabeça_leitura(arq:io.TextIOWrapper):
+    '''
+    Retorna a cabeça de leitura para o início do arquivo
+    '''
+    arq.seek(4)
+
 def importa_filmes() -> io.TextIOWrapper:
     '''
     Abre o arquivo de filmes e o retorna
@@ -19,7 +26,7 @@ def importa_filmes() -> io.TextIOWrapper:
     # Abre o arquivo no modo de leitura binário
     arq:io.TextIOWrapper = open("filmes.dat","rb")
     # Move a cabeça de leitura para frente do cabeçalho
-    arq.seek(4)
+    redefinir_cabeça_leitura(arq)
     return arq
 
 
@@ -28,16 +35,37 @@ def le_filme(arq:io.TextIOWrapper) -> Filme:
     Lê o pŕoximo filme da sequência do arquivo 'arq' e o retorna
     ''' 
     tam_reg = int.from_bytes(arq.read(2))
-    filme = arq.read(tam_reg).decode('utf8','strict')
-    campos = filme.split('|')
-    return Filme(campos[0], campos[1], campos[2], campos[3], campos[4], campos[5], campos[6])
+    if(tam_reg):
+        filme = arq.read(tam_reg).decode('utf8','strict')
+        campos = filme.split('|')
+        return Filme(int(campos[0]), campos[1], campos[2], int(campos[3]), campos[4], int(campos[5]), campos[6])
+    else:
 
+        return None
+
+def busca_filme(arq:io.TextIOWrapper,id:int) -> Filme:
+    '''
+    Faz uma busca sequencial no 'arq' procurando pelo filme de
+    'id'. Retorna o filme encontrado ou None caso não seja encontrado.
+    '''
+    redefinir_cabeça_leitura(arq) # Retornando ao início do arquivo
+    filme:Filme = le_filme(arq)
+
+    while(filme):
+        if(filme.id == id):
+            return filme
+        filme = le_filme(arq)
+    print("Filme não encontrado!")
+    return None
 
 def main():
     arq = importa_filmes()
     
-    filme:Filme = le_filme(arq)
-    print(filme.titulo)
+    filme:Filme = busca_filme(arq,78)
+    if(filme):
+        print(filme)
+    else:
+        print("Filme não encontrado")
 
 if __name__ == "__main__":
     main()
